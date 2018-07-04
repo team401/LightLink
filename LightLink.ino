cd219474d8f4a5ae12b63f9f89afad94d5e0055a
Cameron <camearle20@me.com>
2018-07-04 10:55:14 -0400

    Migrated project to a single ino file

--- LightLink.ino
+++ LightLink.ino
@@ -1,13 +1,258 @@
+// Includes
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+
+// Word definitions
+#define START 0x00 //The commmand starting character
+#define END 0xFF //The command ending character
+
+// Color characters
+#define RED 0x01
+#define ORANGE 0x02
+#define YELLOW 0x03
+#define GREEN 0x04
+#define BLUE 0x05
+#define VIOLET 0x06
+#define WHITE 0x07
+#define BLACK 0x08
+
+// Speed characters
+#define SLOW 0x01
+#define FAST 0x02
+
+// Action characters
+#define SOLID 0x01
+#define BLINK 0x02
+#define SIGNAL 0x03
+#define RACE 0x04
+#define BOUNCE 0x05
+#define SPLIT 0x06
+#define BREATHE 0x07
+#define RAINBOW 0x08
+
+// Speeds and Numbers
+#define SLOW_BLINK_SPEED 300
+#define FAST_BLINK_SPEED 75
+#define NUM_BLINKS 5
+#define SLOW_RACE_SPEED 30
+#define FAST_RACE_SPEED 15
+#define SLOW_BOUNCE_SPEED 30
+#define FAST_BOUNCE_SPEED 15
+#define SLOW_SPLIT_SPEED 60
+#define FAST_SPLIT_SPEED 30
+#define SLOW_BREATHE_SPEED 6
+#define FAST_BREATHE_SPEED 3
+#define SLOW_RAINBOW_SPEED 6
+#define FAST_RAINBOW_SPEED 2
+
+// States
+#define STATE_OFF 0
+#define STATE_ON 1
+#define STATE_FORWARD 0
+#define STATE_REVERSE 1
+
+
+// Strip class
+class Strip {
+public:
+  void init(uint8_t pin, uint16_t length_, neoPixelType type = NEO_GRBW + NEO_KHZ800) {
+  	initialized = true;
+	length = length_;
+	pixels = Adafruit_NeoPixel(length, pin, type);
+	pixels.begin();
+	showPixels();
+  }
+  void setAll(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  	if (initialized) {
+		for (int i = 0; i < length; i++) {
+			pixels.setPixelColor(i, r, g, b, w);
+		}
+	}
+  }
+  void update() {
+  	if (initialized) {
+		lastUpdated = millis();
+		switch (color) {
+		case RED:
+			doAction(255, 0, 0, 0);
+			break;
+		case ORANGE:
+			doAction(255, 32, 0, 0);
+			break;
+		case YELLOW:
+			doAction(255, 128, 0, 0);
+			break;
+		case GREEN:
+			doAction(0, 255, 0, 0);
+			break;
+		case BLUE:
+			doAction(0, 0, 255, 0);
+			break;
+		case VIOLET:
+			doAction(255, 0, 255, 0);
+			break;
+		case WHITE:
+			doAction(0, 0, 0, 255);
+			break;
+		case BLACK:
+			doAction(0, 0, 0, 0);
+			break;
+		}
+		showPixels();
+	}
+  }
+  uint16_t length;
+  uint8_t color = BLACK;
+  uint8_t action = SOLID;
+  uint8_t speed = SLOW;
+  uint8_t lastAction = SOLID;
+  uint8_t colorFromSignal = BLACK;
+  uint8_t actionFromSignal = SOLID;
+  uint8_t speedFromSignal = SLOW;
+    bool state = 0;
+  unsigned long lastUpdated = 0;
+  unsigned long next = 0;
+  long counter = 0;
+  Adafruit_NeoPixel pixels;
+  void showPixels() { pixels.show(); }
+  void setBrightness(uint8_t brightness) { pixels.setBrightness(brightness); }
+private:
+  bool initialized = false;
+    uint32_t wheel(uint8_t pos) {
+  	pos = 255 - pos;
+	if (pos < 85) {
+		return pixels.Color(255 - pos * 3, 0, pos * 3);
+	}
+	if (pos < 170) {
+		pos -= 85;
+		return pixels.Color(0, pos * 3, 255 - pos * 3);
+	}
+	pos -= 170;
+	return pixels.Color(pos * 3, 255 - pos * 3, 0);
+  }
+  void doAction(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  if (action != lastAction) {
+		lastAction = action;
+		counter = -1;
+		state = 0;
+		next = 0;
+	}
+	switch (action) {
+	case SOLID:
+		counter = 0;
+		setAll(r, g, b, w);
+		break;
+	case BLINK:
+	case SIGNAL:
+		if (lastUpdated > next) { //It's time to schedule a new blink
+			state = !state;
+			if (speed == SLOW) next = lastUpdated + SLOW_BLINK_SPEED;
+			if (speed == FAST || action == SIGNAL) next = lastUpdated + FAST_BLINK_SPEED;
+			if (action == SIGNAL && state == STATE_ON) counter++;
+		}
+		if (counter > NUM_BLINKS) { //Blink is over
+			state = STATE_OFF;
+			counter = 0;
+			color = colorFromSignal;
+			action = actionFromSignal;
+			speed = speedFromSignal;
+		}
+		if (state) {
+			setAll(r, g, b, w);
+		} else {
+			setAll(0, 0, 0, 0);
+		}
+		break;
+	case RACE:
+		if (lastUpdated > next) {
+			if (speed == SLOW) next = lastUpdated + SLOW_RACE_SPEED;
+			if (speed == FAST) next = lastUpdated + FAST_RACE_SPEED;
+			counter++;
+		}
+		if (counter >= length) {
+			counter = 0;
+		}
+		setAll(0, 0, 0, 0);
+		pixels.setPixelColor(counter, r, g, b, w);
+		break;
+	case BOUNCE:
+		if (lastUpdated > next) {
+			if (speed == SLOW) next = lastUpdated + SLOW_BOUNCE_SPEED;
+			if (speed == FAST) next = lastUpdated + FAST_BOUNCE_SPEED;
+			counter++;
+		}
+		if (counter >= length) {
+			counter = 0;
+			state = !state;
+		}
+		setAll(0, 0, 0, 0);
+		if (state) {
+			pixels.setPixelColor((length - 1) - counter, r, g, b, w);
+		} else {
+			pixels.setPixelColor(counter, r, g, b, w);
+		}
+		break;
+	case SPLIT:
+		if (lastUpdated > next) {
+			if (speed == SLOW) next = lastUpdated + SLOW_SPLIT_SPEED;
+			if (speed == FAST) next = lastUpdated + FAST_SPLIT_SPEED;
+			counter++;
+		}
+		setAll(0, 0, 0, 0);
+		if (length % 2 == 0) {
+			if (counter >= length / 2) {
+				counter = 0;
+			}
+			pixels.setPixelColor(length / 2 - counter - 1, r, g, b, w);
+			pixels.setPixelColor(length / 2 + counter, r, g, b, w);
+		} else {
+			if (counter > length / 2) {
+				counter = 0;
+			}
+			pixels.setPixelColor(length / 2 + counter, r, g, b, w);
+			pixels.setPixelColor(length / 2 - counter, r, g, b, w);
+		}
+		break;
+	case BREATHE:
+		if (lastUpdated > next) {
+			if (speed == SLOW) next = lastUpdated + SLOW_BREATHE_SPEED;
+			if (speed == FAST) next = lastUpdated + FAST_BREATHE_SPEED;
+			counter++;
+		}
+		if (counter >= 255) {
+			counter = 0;
+			state = !state;
+		}
+		if (state) {
+			setAll(map(255 - counter, 0, 255, 0, r), map(255 - counter, 0, 255, 0, g), map(255 - counter, 0, 255, 0, b), map(255 - counter, 0, 255, 0, w));
+		} else {
+			setAll(map(counter, 0, 255, 0, r), map(counter, 0, 255, 0, g), map(counter, 0, 255, 0, b), map(counter, 0, 255, 0, w));
+		}
+		break;
+	case RAINBOW:
+		if (lastUpdated > next) {
+			if (speed == SLOW) next = lastUpdated + SLOW_RAINBOW_SPEED;
+			if (speed == FAST) next = lastUpdated + FAST_RAINBOW_SPEED;
+			counter++;
+		}
+		if (counter >= 255) {
+			counter = 0;
+		}
+		for (int i = 0; i < length; i++) {
+			pixels.setPixelColor(i, wheel(((i * 256 / length) + counter) & 255));
+		}
+		break;
+	}
+  }
+};
+
+// ino start
+
 /*
  Name:		LightLink.ino
  Created:	9/21/2017 8:11:22 AM
  Author:	cameronearle
 */
-
-#include <Wire.h>
-#include <Adafruit_NeoPixel.h>
-#include "Strip.h"
-#include "Definitions.h"
 
 uint8_t command[4]; //Array to hold the command being built
 uint8_t cursor = 0; //Cursor to track array position
